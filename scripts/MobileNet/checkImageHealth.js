ffc035671e31a7c47cfd6d614e236ef1e4919b8e
Kevin Scott <kevin@hitheory.com>
2018-11-27 10:47:36 -0500

    Update scripts

--- scripts/MobileNet/checkImageHealth.js
+++ scripts/MobileNet/checkImageHealth.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const request = require('request');
+const dns = require('dns');
 
 const {
   ALL_IMAGES,
   IMAGE_HEALTH,
+  PARALLEL,
+  TIMEOUT,
 } = require('./config');
 
 const imagesAll = require(ALL_IMAGES);
@@ -36,7 +39,7 @@ const check = (uri) => new Promise((resolve, reject) => {
 
   setTimeout(() => {
     r(() => reject(`Timeout error for ${uri}`));
-  }, 10000);
+  }, TIMEOUT);
 
 
   request.head(uri, (err, res) => {
@@ -58,8 +61,10 @@ const check = (uri) => new Promise((resolve, reject) => {
   });
 });
 
-(async function() {
+const main = async () => {
   const entries = Object.entries(imagesAll);
+  const callbacks = [];
+
   for (let i = 0; i < entries.length; i++) {
     const [
       key,
@@ -70,30 +75,81 @@ const check = (uri) => new Promise((resolve, reject) => {
       imageHealth[key] = {};
     }
 
-    for (let j = 0; j < values.length; j++) {
-      if (imageHealth[key][j] === undefined) {
-        console.log(key, j);
-        const url = values[j];
-        try {
-          await check(url);
-          imageHealth[key] = {
-            ...imageHealth[key],
-            [j]: 1,
-          };
-        } catch(err) {
-          console.error(err);
-          imageHealth[key] = {
-            ...imageHealth[key],
-            [j]: 0,
-          };
-        }
+    for (let index = 0; index < values.length; index++) {
+      if (imageHealth[key][index] === undefined) {
+        callbacks.push({
+          key,
+          index,
+          values,
+        });
       }
-
-      fs.writeFileSync(
-        IMAGE_HEALTH,
-        JSON.stringify(imageHealth)
-      );
     }
   }
-})();
 
+  console.log(`${callbacks.length} fns in total`);
+
+  parallelize(PARALLEL, callbacks.map(({
+    key,
+    index,
+    values,
+  }, i) => async () => {
+    try {
+      await checkNetworkConnectivity();
+    } catch(err) {
+      return;
+    }
+    const url = values[index];
+    console.log([
+      callbacks.length - i,
+      key,
+      index,
+    ].join(' | '));
+    try {
+      await check(url);
+      imageHealth[key] = {
+        ...imageHealth[key],
+        [index]: 1,
+      };
+    } catch(err) {
+      console.error(err);
+      imageHealth[key] = {
+        ...imageHealth[key],
+        [index]: 0,
+      };
+    }
+
+    fs.writeFileSync(
+      IMAGE_HEALTH,
+      JSON.stringify(imageHealth)
+    );
+  }));
+};
+
+const wait = (ms, id) => new Promise(resolve => {
+  console.log('start', id);
+  setTimeout(() => {
+    console.log('complete', id);
+    resolve();
+  }, ms);
+});
+
+const checkNetworkConnectivity = () => new Promise((resolve, reject) => dns.resolve('www.google.com', (err) => {
+  if (err) {
+    reject();
+  } else {
+    resolve();
+  }
+}));
+
+const parallelize = async (parallel, callbacks) => {
+  for (let i = 0; i < parallel; i++) {
+    (async () => {
+      while (callbacks.length > 0) {
+        const fn = callbacks.shift();
+        await fn();
+      }
+    })();
+  }
+};
+
+main();
--- scripts/MobileNet/config.js
+++ scripts/MobileNet/config.js
@@ -8,3 +8,5 @@ module.exports.ALL_IMAGES = `${DATA_ROOT}/all-images.json`;
 module.exports.IMAGE_HEALTH = `${DATA_ROOT}/image-health.json`;
 module.exports.NUM_TO_IDS = `${DATA_ROOT}/num-to-ids.json`;
 module.exports.IMAGES = `${DATA_ROOT}/images.json`;
+module.exports.PARALLEL = 10;
+module.exports.TIMEOUT = 20000;
