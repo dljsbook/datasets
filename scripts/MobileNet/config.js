20d52ccaf78ed6b6cc5b73d4b85392b5946589b5
Kevin Scott <kevin@hitheory.com>
2018-11-30 23:03:57 -0500

    Update images

--- scripts/MobileNet/config.js
+++ scripts/MobileNet/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const DATA_ROOT = path.resolve(__dirname, '../../data/ImageNet');
 
 module.exports.DATA_ROOT = DATA_ROOT;
-module.exports.MAX_NUM_IMAGES = 1000;
+module.exports.MAX_NUM_IMAGES = 10;
 module.exports.ALL_IMAGES = `${DATA_ROOT}/all-images.json`;
 module.exports.IMAGE_HEALTH = `${DATA_ROOT}/image-health.json`;
 module.exports.NUM_TO_IDS = `${DATA_ROOT}/num-to-ids.json`;
