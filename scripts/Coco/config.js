dea2e99ca3dec951b7d163e625ae88bb1a8bf5c6
Kevin Scott <kevin@hitheory.com>
2018-12-03 09:44:47 -0500

    Bring config back even lower

--- scripts/Coco/config.js
+++ scripts/Coco/config.js
@@ -5,7 +5,7 @@ const path = require('path');
 const DATA_ROOT = path.resolve(__dirname, '../../data/Coco');
 
 module.exports.DATA_ROOT = DATA_ROOT;
-module.exports.PER_FILE = yargs.perFile || 1000;
+module.exports.PER_FILE = yargs.perFile || 100;
 module.exports.FILES = yargs.files || 10;
 module.exports.MANIFEST = `images/manifest.json`;
 module.exports.CATEGORIES = `categories.json`;
